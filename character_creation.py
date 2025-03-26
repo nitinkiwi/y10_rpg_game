2b121eca5c8fd46e8a3e83a24f03e2c9d7733b74
Nitin Keswani <nitinjameskeswani@gmail.com>
2025-03-27 07:27:42 +1300

    Made a file for character creation

---
+++ character_creation.py
@@ -0,0 +1,29 @@
+player_stats = {
+    'Name':'none',
+    'Species':'none',
+    'Health':10,
+    'Damage':10,
+    'Healing':10,
+    'Gold':0, 
+    'Ability':'dodge',
+}
+
+species = ['human', 'dwarf','wizard', 'sigma']
+
+health_multipliers = {
+    'human':12,
+    'dwarf':8,
+    'wizard':12.1,
+    'sigma':13
+}
+
+damage_multipliers = {
+    'human':5,
+    'dwarf':8,
+    'wizard':6,
+    'sigma':6
+}
+
+stats_chosen = False
+
+double_damage_multiplier = 2
